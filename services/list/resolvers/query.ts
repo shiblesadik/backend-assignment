443963e406dd501012e5bc478254091a07c2a269
shiblesadik <shiblesadik@outlook.com>
2022-12-05 00:02:41 +0600

    sorting list by index

--- services/list/resolvers/query.ts
+++ services/list/resolvers/query.ts
@@ -2,7 +2,8 @@ import { Resolvers } from 'generated/types'
 import { Context } from '../../../libs/context'
 
 export const query: Resolvers<Context>['Query'] = {
-  lists: async (_parent, _args, ctx) => ctx.prisma.list.findMany({ include: { tasks: true } }),
+  lists: async (_parent, _args, ctx) => (await ctx.prisma.list.findMany({ include: { tasks: true } }))
+    .sort((a, b) => a.index >= b.index ? 1 : -1),
   list: async (_parent, { id }, ctx) =>
     ctx.prisma.list.findUnique({
       where: { id },
