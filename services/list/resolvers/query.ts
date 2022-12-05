f6d3fe16b6352ff9d50672bcd205c37f978a4add
Shible Sadik <shiblesadik.atm@gmail.com>
2022-12-05 11:53:28 +0600

    Return query results as manually sorting order

--- services/list/resolvers/query.ts
+++ services/list/resolvers/query.ts
@@ -1,9 +1,19 @@
-import { Resolvers } from 'generated/types'
+import { Resolvers, Task } from 'generated/types'
 import { Context } from '../../../libs/context'
 
 export const query: Resolvers<Context>['Query'] = {
   lists: async (_parent, _args, ctx) => {
-    return await ctx.prisma.list.findMany({ include: { tasks: true } });
+    const lists = await ctx.prisma.list.findMany({ include: { tasks: true } });
+    for (let i = 0; i < lists.length; ++i) {
+      const list = lists[i] as any;
+      const tasks = [];
+      for (let j = 0; j < list?.tasksOrder.length; ++j) {
+        const task = list?.tasks.find((task: any) => task.id == list?.tasksOrder[j]);
+        tasks.push(task);
+      }
+      list['tasks'] = tasks;
+    }
+    return lists;
   },
   list: async (_parent, { id }, ctx) =>
     ctx.prisma.list.findUnique({
